5e63ebba5944bf7a9fb4d47af6a0b42d017a2651
CFCode-git <435184766@qq.com>
2020-01-23 16:03:22 +0800

    创建 Layout 组件

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,11 +1,16 @@
 <template>
-        <Layout><p>Statistics.vue</p></Layout>
+    <div class="nav-wrapper">
+        <div class="content">
+            <slot/>
+        </div>
+        <Nav/>
+    </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Statistics',
-  };
+    export default {
+      name:'Layout'
+    }
 </script>
 
 <style lang="scss" scoped>
@@ -19,6 +24,6 @@
     .content {
         flex-grow: 1;
         overflow: auto;
-        border: 1px solid blue;
+        border:1px solid blue;
     }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,7 @@
 <template>
-    <div class="nav-wrapper">
-        <div class="content">
-            <p>Money.vue</p>
-        </div>
-        <Nav/>
-    </div>
+    <Layout>
+        <p>Money.vue</p>
+    </Layout>
 </template>
 
 <script lang="ts">
@@ -12,18 +9,3 @@
     name: 'money',
   };
 </script>
-
-<style lang="scss" scoped>
-    .nav-wrapper {
-        display: flex;
-        border: 1px solid green;
-        flex-direction: column;
-        height: 100vh;
-    }
-
-    .content {
-        flex-grow: 1;
-        overflow: auto;
-        border:1px solid blue;
-    }
-</style>

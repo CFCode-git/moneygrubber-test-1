ce3d0fc143aa995919245e231d5b36ffa1b97342
CFCode-git <435184766@qq.com>
2020-01-24 15:00:13 +0800

    创建 <Icon />

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,29 @@
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+
+    config.module
+      .rule('svg-sprite')       /* 添加要遵守的规则*/
+      .test(/\.svg$/)
+      .include.add(dir).end() // 只包含 icons 目录
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end() // 配置loader
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}]) // 配置插件plugin
+    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons目录
+
+    // .test(/\.(svg)(\?.*)?$/)
+    //   .include.add(dir).end()
+    //   .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
+    //   .use('svgo-loader').loader('svgo-loader')
+    //   .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
+    //   .end()
+    // config.plugin('svg.sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
+    // config.module.rule('svg').exclude.add(dir)
+  }
+
 }
+
+

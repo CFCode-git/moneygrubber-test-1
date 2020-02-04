42c327b55fcd043e61bbd8a42b1460ad49611677
CFCode-git <435184766@qq.com>
2020-02-04 15:22:44 +0800

    添加全局样式和全局变量

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="app">
         <router-view/>
         <hr/>
 
@@ -7,37 +7,13 @@
 </template>
 
 <style lang="scss">
-    * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-    }
-
+    @import "~@/assets/style/reset.scss";
+    @import "~@/assets/style/helper.scss";
     body{
-        line-height: 1.5;
-    }
-    a{
-        text-decoration:none;
-        color:inherit;
-    }
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
-
-    #nav {
-        padding: 30px;
-
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-
-            &.router-link-exact-active {
-                color: #42b983;
-            }
-        }
+        color: #333;
+        line-height: 1.5;
+        font-family: $font-hei;
     }
 </style>

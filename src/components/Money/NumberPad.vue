eb4bdf19573675ed100b29d5eb62618a641cfb83
CFCode-git <435184766@qq.com>
2020-02-07 17:49:30 +0800

    收集四个组件的数据到record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -22,11 +22,12 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component,Prop} from 'vue-property-decorator';
 
   @Component
   export default class NumberPad extends Vue {
-    output: String = '0';
+    @Prop() readonly value!: number;
+    output: String = this.value.toString();
 
     inputContent(event: MouseEvent) {
       const button = (event.target as HTMLButtonElement);
@@ -53,11 +54,12 @@
     }
 
     clear() {
-        this.output = '0'
+      this.output = '0';
     }
 
-    ok() {}
-
+    ok() {
+      this.$emit('update:value', this.output);
+    }
   }
 
 </script>

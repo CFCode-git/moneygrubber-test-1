5d3c9d4b4130fe0ead7276b0bc10c105b0625f51
CFCode-git <435184766@qq.com>
2020-02-07 22:26:11 +0800

    添加 createAt 字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
     <Layout class-prefix="layout">
-        {{record}}
         <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
         <Notes @update:value="onUpdateNotes"/>
@@ -16,11 +15,15 @@
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
 
+
+  const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+
   type Record = {
     tags: string[]
     notes: string
     type: string
-    amount: number
+    amount: number // 数据类型 object | number。。。
+    createAt?: Date // 类 / 构造函数
   }
 
   @Component({
@@ -29,7 +32,7 @@
   export default class Money extends Vue {
     tags = ['衣', '食', '住', '行'];
     record: Record = {tags: [], notes: '', type: '-', amount: 0};
-    recordList: Record[] = [];
+    recordList: Record[] = recordList;
 
     onUpdateTags(value: string[]) {
       this.record.tags = value;
@@ -41,9 +44,9 @@
 
     saveRecord() {
       // 深拷贝一下
-      const record2 = JSON.parse(JSON.stringify(this.record));
+      const record2: Record = JSON.parse(JSON.stringify(this.record));
+      record2.createAt = new Date();
       this.recordList.push(record2);
-      console.log(this.recordList);
     }
 
     @Watch('recordList')

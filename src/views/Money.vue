127034d148b3bda61583a03553a5c2fd714e19d1
CFCode-git <435184766@qq.com>
2020-02-09 12:48:31 +0800

    添加  EditLabel.vue 的样式，抽离了两个组件 Button 和 formItem

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,11 @@
     <Layout class-prefix="layout">
         <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
-        <Notes field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
+        <div class="FromItem-wrapper">
+            <FromItem field-name="备注"
+                      placeholder="在这里输入备注"
+                      @update:value="onUpdateNotes"/>
+        </div>
         <Tags :data-source.sync="tags" @update:selected="onUpdateTags"/>
     </Layout>
 </template>
@@ -12,7 +16,7 @@
   import {Component, Watch} from 'vue-property-decorator';
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
+  import FromItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {recordListModel} from '@/models/recordListModel';
   import {tagListModel} from '@/models/tagListModel';
@@ -21,7 +25,7 @@
   const tagList = tagListModel.fetch();
 
   @Component({
-    components: {Tags, Notes, Types, NumberPad},
+    components: {Tags, FromItem, Types, NumberPad},
   })
   export default class Money extends Vue {
     tags = tagList;
@@ -55,5 +59,9 @@
     .layout-content {
         display: flex;
         flex-direction: column-reverse;
+        background: #f5f5f5;
+    }
+    .FromItem-wrapper{
+        padding:12px 0;
     }
 </style>

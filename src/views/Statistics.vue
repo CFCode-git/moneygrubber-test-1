2a14ce25dc1fe242d3bf604db93f58a3009746b6
CFCode-git <435184766@qq.com>
2020-02-21 20:14:27 +0800

    抽离 Tabs.vue, 使用在 Statistics.vue 里面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,24 +1,53 @@
 <template>
-        <Layout><p>Statistics.vue</p></Layout>
+    <Layout>
+        <Tabs class-prefix="type"
+              :data-source="typeList"
+              :value.sync="type"/>
+        <Tabs class-prefix="interval"
+              :data-source="intervalList"
+              :value.sync="interval"/>
+        <div>
+            type:{{type}}
+            <br>
+            interval:{{interval}}
+        </div>
+    </Layout>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Statistics',
-  };
+  import Types from '@/components/Money/Types.vue';
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  import Tabs from '@/components/Tabs.vue';
+
+  @Component({
+    components: {Tabs, Types},
+  })
+  export default class Statistics extends Vue {
+    type = '-';
+    interval = 'day';
+    intervalList = [
+      {text: '按天', value: 'day'},
+      {text: '按周', value: 'week'},
+      {text: '按月', value: 'month'}
+    ];
+    typeList = [
+      {text:'支出',value:'-'},
+      {text:'收入',value:'+'},
+    ]
+  }
 </script>
 
 <style lang="scss" scoped>
-    .nav-wrapper {
-        display: flex;
-        border: 1px solid green;
-        flex-direction: column;
-        height: 100vh;
-    }
+    ::v-deep .type-tabs-item {
+        background: white;
 
-    .content {
-        flex-grow: 1;
-        overflow: auto;
-        border: 1px solid blue;
+        &.selected {
+            background: #c4c4c4;
+
+            &::after {
+                display: none;
+            }
+        }
     }
 </style>
